ce15c14f86b4bc20e2263ca0deb4a85d3c0bf32a
Aditya K G <akarnam37@gmail.com>
2016-07-16 16:36:36 +0530

    Initial Commit

---
+++ application/config/digitalocean_api.php
@@ -0,0 +1,18 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/*
+ * Digital Ocean API File Config
+ *
+ * File     : digitalocean_api.php
+ * Created  : Sat 16th July, 2016 | 11:05:00 AM
+ * 
+ * Author   : Aditya Karnam <akarnam37@gmail.com>
+ */
+//Instamojo API Key and Auth Token
+
+//Website ; Enter your website without www
+$config['website']   = 'YOUR WEBSITE';
+
+//Auth/Access Token ; Generate your access token from Digital Ocean Dashboard
+$config['auth_token']  = 'YOUR AUTH TOKEN';
+
+?>
